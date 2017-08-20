a47c4b57629f1c3077abe9be074a62a20509ae9e
Nelson Eldoro <nelsoneldoro@MacBook-Pro-de-Nelson-Eldoro.local>
2017-08-20 10:27:51 -0300

    create basic next project

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _jscReactUi = require('jsc-react-ui');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/nelsoneldoro/Development/next/notes/pages/index.js?entry';
+
+
+var ButtonExample = function ButtonExample() {
+  return _react2.default.createElement(_jscReactUi.Button, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  });
+};
+
+exports.default = ButtonExample;
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Button } from 'jsc-react-ui';
+
+const ButtonExample = () => <Button />;
+
+export default ButtonExample
