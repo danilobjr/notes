2b0d36d237f256a35d1aaa9b9883cff572a0bb45
Nelson Eldoro <nelsoneldoro@MacBook-Pro-de-Nelson-Eldoro.local>
2017-08-20 11:48:51 -0300

    setup typescript

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Button } from 'jsc-react-ui';
-
-const ButtonExample = () => <Button />;
-
-export default ButtonExample => (
-    <div>Welcome to next.js! <Button /></div>
-  )
