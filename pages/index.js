2cced05a6ce67ccd83a8be26cfb2c02b75e6f862
Nelson Eldoro <nelsoneldoro@MacBook-Pro-de-Nelson-Eldoro.local>
2017-08-20 10:38:33 -0300

    remove next ignored files

--- pages/index.js
+++ pages/index.js
@@ -3,4 +3,6 @@ import { Button } from 'jsc-react-ui';
 
 const ButtonExample = () => <Button />;
 
-export default ButtonExample
+export default ButtonExample => (
+    <div>Welcome to next.js! <Button /></div>
+  )
