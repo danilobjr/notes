4fef9f09ab0b29eb487bf0d2f722576a53bc7f6c
Nelson Eldoro <nelsoneldoro@MacBook-Pro-de-Nelson-Eldoro.local>
2017-08-21 21:53:25 -0300

    Refact styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import {Component} from 'react';
+import {CSSProperties} from 'react';
 import {Card} from 'jsc-react-ui';
 
-const cardStyle = {
-  width: 246,
+const styles = {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+  } as CSSProperties,
+  card: {
+    width: 246,
+  }
 };
 
 //não consegui passar este estilo na div
@@ -15,15 +22,15 @@ const wrapperStyle = {
 export default class Index extends Component {
   render() {
     return (
-        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={styles.wrapper}>
           <Card
-            style={cardStyle}
+            style={styles.card}
             title="Card 1"
             image="https://facebook.github.io/react/img/logo.svg">
               Sed tempus, odio nec laoreet pulvinar.
           </Card>
           <Card
-            style={cardStyle}
+            style={styles.card}
             title="Card 2"
             image="https://facebook.github.io/react/img/logo.svg">
               Sed tempus, odio nec laoreet pulvinar.
