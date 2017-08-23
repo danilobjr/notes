2f3c211262af294b549c249207050a243d3931b6
Nelson Eldoro <nelsoneldoro@MacBook-Pro-de-Nelson-Eldoro.local>
2017-08-22 23:13:31 -0300

    feat(filter): Filter cards by title
    Filter cards by typing on search input.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import {Component, CSSProperties} from 'react';
-import {Card} from 'jsc-react-ui';
+import {Card, FloatingInput, Icon} from 'jsc-react-ui';
 
 const styles = {
   wrapper: {
     display: 'flex',
     padding: 10,
-    flexWrap: 'wrap',
+    flexWrap: 'wrap'
   } as CSSProperties,
   card: {
     width: 246,
     margin: '25px 0 0 25px',
+  },
+  base: {
+    width: 246,
+  },
+  icon: {
+    margin: '0 14px 0 18px',
   },
 };
 
@@ -22,23 +28,37 @@ interface CardModel {
 
 interface State {
   cards: CardModel[];
+  filteredCards: CardModel[];
 }
 
 export default class Index extends Component<{}, State> {
 
   state: State = {
     cards: [
-      {id:1, title: "Example title1", text: "Example Content"},
-      {id:2, title: "Example title2", text: "Example Content"},
+      {id:1, title: "Fazer listagem", text: "listar os cards"},
+      {id:2, title: "Filtrar listagem", text: "exibir os cards de acordo com a busca"},
+      {id:3, title: "Adicionar cards", text: "criar form em modal"}
     ],
+    filteredCards: [
+      {id:1, title: "Fazer listagem", text: "listar os cards"},
+      {id:2, title: "Filtrar listagem", text: "exibir os cards de acordo com a busca"},
+      {id:3, title: "Adicionar cards", text: "criar form em modal"}
+    ]
   };
 
   render() {
-    const {cards} = this.state;
+    const {filteredCards} = this.state;
     
     return (
         <div style={styles.wrapper}>
-          {cards.map(c => 
+            <FloatingInput
+              style={styles.base}
+              placeholder="Search"
+              leftElement={<Icon style={styles.icon}
+              name="search" />}
+              onChange={this.handleChange}
+            />
+          {filteredCards.map(c => 
             <Card
               key={c.id}
               style={styles.card}
@@ -49,5 +69,15 @@ export default class Index extends Component<{}, State> {
           )}
         </div>
     );
+  }
+
+  handleChange = (event) => {
+    const {value} = event.target;
+
+    const cards = this.state.cards.filter((card) => {
+      return card.title.toUpperCase().includes(value.toUpperCase());
+    })
+
+    this.setState({filteredCards: cards});
   }
 }
