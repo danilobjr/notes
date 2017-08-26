da1836eeaf2f7ee552c84e84eb3699aba6901c00
Nelson Eldoro <nelsoneldoro@MacBook-Pro-de-Nelson-Eldoro.local>
2017-08-25 22:20:07 -0300

    feat(add): Open modal and add card

---
+++ src/components/CardForm.tsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import {Component, CSSProperties} from 'react';
+import {Modal, ModalHeader, ModalBody, ModalFooter, Button} from 'jsc-react-ui';
+import {Note} from './../types/Note';
+
+const styles = {
+  modal: {
+    width: 500,
+    height: 400,
+    backgroundColor: '#fff',
+    display: 'flex',
+    flexFlow: 'column',
+  },
+  input: {
+    border: 'none',
+    fontWeight: 600,
+    fontSize: '1.1em',
+    flex: 1,
+  } as CSSProperties,
+  textarea: {
+    border: 'none',
+    resize: 'none',
+    flex: 1,
+  },
+};
+
+interface Props {
+  open: boolean;
+  onSave?: (note: Note) => void;
+  onClose?: () => void;
+}
+
+export class CardForm extends Component<Props, Note> {
+
+  props: Props = {
+    open: false,
+  };
+
+  state: Note = this.getInitialState();
+
+  render() {
+    const {open} = this.props;
+
+    return (
+      <div>
+        <Modal
+          style={styles.modal}
+          noBackdrop
+          open={open}
+          onClose={this.props.onClose}
+        >
+          <ModalHeader>
+            <input
+              style={styles.input}
+              type="text"
+              placeholder="Title"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleInputChange}
+            />
+          </ModalHeader>
+
+          <ModalBody>
+            <textarea
+              style={styles.textarea}
+              placeholder="Write some thoughts."
+              name="text"
+              value={this.state.text}
+              onChange={this.handleInputChange}
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button flat onClick={this.handleSaveButtonClick}>Save</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+
+  handleInputChange = (event: any) => {
+    const {target} = event;
+    const {value} = target;
+    const {name} = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSaveButtonClick = () => {
+    if (this.state.title !== '') {
+      this.props.onSave(this.state);
+      this.setState(this.getInitialState());
+    } else {
+      alert('Title is mandatory');
+    }
+  }
+
+  getInitialState() {
+    return {
+      id : +new Date(),
+      title : '',
+      text : '',
+    };
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,34 @@
 import * as React from 'react';
-import {Component} from 'react';
-import {FloatingInput, Icon} from 'jsc-react-ui';
+import {Component, CSSProperties} from 'react';
+import {FloatingInput, Icon, IconButton} from 'jsc-react-ui';
 import {CardList} from './../components/CardList';
+import {CardForm} from './../components/CardForm';
 import {Note} from './../types/Note';
 
 const styles = {
   container: {
     padding: 10,
   },
+  actionContainer: {
+    display: 'flex',
+    justifyContent : 'space-between',
+    margin: '25px 25px 0 25px',
+  } as CSSProperties,
   filter: {
     width: 246,
-    margin: '25px 0 0 25px',
   },
   icon: {
     margin: '0 14px 0 18px',
+  },
+  iconButton : {
+    backgroundColor: '#F1C40F',
   },
 };
 
 interface State {
   notes: Note[];
   filter: string;
+  modalOpen: boolean;
 }
 
 export default class Index extends Component<{}, State> {
@@ -27,6 +36,7 @@ export default class Index extends Component<{}, State> {
   state: State = {
     notes: [],
     filter: '',
+    modalOpen: false,
   };
 
   componentDidMount() {
@@ -38,17 +48,25 @@ export default class Index extends Component<{}, State> {
   }
 
   render() {
-    const {notes} = this.state;
+    const {notes, modalOpen} = this.state;
 
     return (
       <div style={styles.container}>
-        <FloatingInput
-          style={styles.filter}
-          placeholder="Search"
-          leftElement={<Icon style={styles.icon} name="search" />}
-          onChange={this.handleFilterChange}
-        />
+        <div style={styles.actionContainer}>
+          <FloatingInput
+            style={styles.filter}
+            placeholder="Search"
+            leftElement={<Icon style={styles.icon} name="search" />}
+            onChange={this.handleFilterChange}
+          />
+          <IconButton style={styles.iconButton}
+            iconName="plus"
+            onClick={this.handleNewButtonClick} />
+        </div>
         <CardList cards={this.getFilteredCards()} />
+        <CardForm open={modalOpen}
+          onSave={this.handleOnModalSave}
+          onClose={this.handleOnModalClose} />
       </div>
     );
   }
@@ -64,5 +82,18 @@ export default class Index extends Component<{}, State> {
   handleFilterChange = (event: any) => {
     const {value} = event.target;
     this.setState({filter: value});
+  }
+
+  handleNewButtonClick = () => {
+    this.setState({modalOpen: true});
+  }
+
+  handleOnModalClose = () => {
+    this.setState({modalOpen: false});
+  }
+
+  handleOnModalSave = (note: Note) => {
+    const notes = [...this.state.notes, note];
+    this.setState({notes: notes, modalOpen: false});
   }
 }
